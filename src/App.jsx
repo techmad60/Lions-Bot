acdf3c649e5605b35ff66f0b51d7cbef8f7f720f
Success Obasi <obasisuccess192@gmail.com>
2024-01-29 08:59:57 +0100

    still on it

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
           </ul>
             <div className='flex flex-col mt-10 w-full items-center lg:mt-0 lg:w-fit lg:flex-row lg:gap-6 text-center sm:hidden'>
             <a href="#" className='text-white bg-primary-green p-2 rounded-md w-32 mb-4 lg:mb-0 '>Sign up</a>
-            <a href="#" className=' rounded-md p-2 w-32 login '>Login</a>
+            <a href="#" className='rounded-md p-2 w-32 login'>Login</a>
           </div>
         </nav>
         <div className='lg:flex flex-col mt-10 w-full items-center lg:mt-0 lg:w-fit lg:flex-row lg:gap-6 text-center hidden'>
