90a0ea6adc455ae9f86c2ed2197f1382401fb76b
Success Obasi <obasisuccess192@gmail.com>
2024-02-01 16:14:38 +0100

    better-behave

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx';
 import './index.css';
-import NotfoundPage from './pages/NotfoundPage.jsx';
 import Signup from './pages/SignUpPage.jsx';
 import Login from './pages/LoginPage.jsx';
 
@@ -11,17 +10,14 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
-    errorElement: <NotfoundPage />,
   },
   {
     path: '/signup',
     element: <Signup />,
-    errorElement: <NotfoundPage />,
   },
   {
     path: '/login',
     element: <Login />,
-    errorElement: <NotfoundPage />,
   }
 ]);
 
--- src/pages/NotFoundPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotfoundPage = () => {
-  return (
-    <div>NotfoundPage</div>
-  )
-}
-
-export default NotfoundPage
